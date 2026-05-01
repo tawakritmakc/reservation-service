1c386d64ebfb66c8aff3e46d0559cb1e351f3fa9
tawakritmakc <tawakrit.makc@dome.tu.ac.th>
2026-05-01 09:54:33 +0700

    Change Kafka client settings for quotation service
    Updated Kafka client configuration for quotation service with SSL and SASL authentication.

--- src/kafka/index.js
+++ src/kafka/index.js
@@ -1,8 +1,15 @@
 const { Kafka } = require('kafkajs');
 
 const kafka = new Kafka({
-  clientId: 'reservation-service',
-  brokers: (process.env.KAFKA_BROKERS || 'localhost:9092').split(','),
+  clientId: 'quotation-service',
+  brokers: ['pkc-619z3.us-east1.gcp.confluent.cloud:9092'],
+  ssl: true,
+  sasl: {
+    mechanism: 'plain',
+    username: process.env.KAFKA_SASL_USERNAME,
+    password: process.env.KAFKA_SASL_PASSWORD,
+  },
+  connectionTimeout: 45000,
 });
 
 const producer = kafka.producer();
